dd5c8a88f757e05793e69bfeea68771c15fb4533
setosun5eth <setosun5@yahoo.co.jp>
2025-10-08 10:03:57 +0900

    12th

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const passport = require('passport');
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegister);
+router.route('/register')
+    .get(users.renderRegister)
+    .post(users.register);
 
-router.post('/register', users.register);
-
-router.get('/login', users.renderLogin);
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
 router.get('/logout', users.logout);
 
