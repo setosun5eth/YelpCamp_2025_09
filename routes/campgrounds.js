f4e8dddad2cc4e9e63d6c7f648ac5f8987ff1e91
setosun5eth <setosun5@yahoo.co.jp>
2025-10-16 08:59:19 +0900

    14th

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,10 +3,16 @@ const campgrounds = require('../controllers/campgrounds');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/'})
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send('受け付けました');
+    });
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
